dd0fda326de0cfd19f6df9dc1d4c91d3b7d368a9
ecoskun87 <ecoskun87@gmail.com>
2024-03-15 12:47:00 +0300

    vs codetan githuba ilk gönderme işlemi.

---
+++ script.js
@@ -0,0 +1,71 @@
+// let mybutton = document.getElementById("mybutton")
+// let silik=false
+// mybutton.onclick=()=>{
+// if(silik) {
+//     document.getElementById("mydiv").innerHTML="Hello World"
+// silik=false
+// }
+// else {
+//     document.getElementById("mydiv").innerHTML=""
+// silik=true
+// }
+// }
+// let sifre=prompt("Şifre Girin")
+
+// console.log(Math.abs(-33));
+// console.log(Math.floor(32.7));
+// console.log(Math.ceil(32.7));
+// console.log(Math.pow(2, 3));
+// console.log(Math.round(3.49));
+
+// let a=Math.trunc(Math.random()*1000)
+
+// document.getElementById("mydiv").innerHTML=a
+
+// // const kisi1 = {ad: "Emir", soyad: "Özdemir", yas: 25};
+// // const kisi2 = {ad: "Ahmet", soyad: "Özdemir2", yas: 45};
+// // const kisi3 = {ad: "Mehmet", soyad: "Özdemir4", yas: 22};
+// // const kisi4 = {ad: "Can", soyad: "Özdemir", yas: 25};
+// // const kisi5 = {ad: "Cem", soyad: "Özdemir", yas: 41};
+
+// // let saylar=new Set([])
+
+// // for (let i=1;i<6;i++) {
+
+// //     let a =prompt("Sayi girin " + i)
+
+// //     saylar.add(a)
+// // }
+
+// // for( sayi of saylar)
+// // {
+// // document.write(sayi)}
+
+// let kisiler = new Map();
+// let id = 0;
+
+// for (let i = 0; i < 5, i++; ) {
+
+//   let hasKisi = false;
+
+//   let kisi = prompt("kullanıcı adı girin : ");
+
+//   kisiler.forEach((it) => {
+//     if (it == kisi) {
+//       hasKisi = true;
+//     }
+//   });
+
+//   if (!hasKisi) {
+//     kisiler.set(id, kisi);
+//   }
+
+//   id = id + 1;
+// }
+
+
+function gizle() {
+
+  document.getElementById("mydiv").style.display="none"
+
+}
